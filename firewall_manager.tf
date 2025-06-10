e16e355ec0c5f381ea078a8b4eda2354ebaecd93
Worxfr <worx@pouf.org>
2025-06-11 00:57:56 +0200

    S3 policy update

--- firewall_manager.tf
+++ firewall_manager.tf
@@ -47,6 +47,31 @@ resource "aws_s3_bucket_policy" "waf_logs" {
           aws_s3_bucket.waf_logs.arn,
           "${aws_s3_bucket.waf_logs.arn}/*"
         ]
+      },
+      {
+        Sid    = "AWSLogDeliveryWriteFMS"
+        Effect = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = [
+          "${aws_s3_bucket.waf_logs.arn}/*/AWSLogs/*"
+        ]
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        Sid    = "AWSLogDeliveryAclCheckFMS"
+        Effect = "Allow"
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.waf_logs.arn
       }
     ]
   })
